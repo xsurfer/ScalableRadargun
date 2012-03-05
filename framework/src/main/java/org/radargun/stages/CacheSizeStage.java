c5778366f5680a6b7128325fe4dea18876683f01
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-03-05 10:29:53 +0000

    * FIX: cache size stat name

--- framework/src/main/java/org/radargun/stages/CacheSizeStage.java
+++ framework/src/main/java/org/radargun/stages/CacheSizeStage.java
@@ -17,75 +17,73 @@ import java.util.List;
  */
 public class CacheSizeStage extends AbstractDistStage {
 
-    private String statName = "CACHE_SIZE_" + System.nanoTime();
+   private String statName = "CACHE_SIZE_" + System.nanoTime();
 
-    private boolean resetOldValues = false;
+   private boolean reset = false;
 
-    @Override
-    public DistStageAck executeOnSlave() {
-        DefaultDistStageAck ack = newDefaultStageAck();
-        CacheWrapper wrapper = slaveState.getCacheWrapper();
-        if (wrapper == null) {
-            log.info("Not executing any test as the wrapper is not set up on this slave ");
-            ack.setError(true);
-            ack.setErrorMessage("Cache Wrapper is null");
-            return ack;
-        }
+   @Override
+   public DistStageAck executeOnSlave() {
+      DefaultDistStageAck ack = newDefaultStageAck();
+      CacheWrapper wrapper = slaveState.getCacheWrapper();
+      if (wrapper == null) {
+         log.info("Not executing any test as the wrapper is not set up on this slave ");
+         ack.setError(true);
+         ack.setErrorMessage("Cache Wrapper is null");
+         return ack;
+      }
 
-        ack.setPayload(wrapper.getCacheSize());
-        ack.setDuration(0);
-        return ack;
-    }
+      ack.setPayload(wrapper.getCacheSize());
+      ack.setDuration(0);
+      return ack;
+   }
 
-    @Override
-    public boolean processAckOnMaster(List<DistStageAck> acks, MasterState masterState) {
-        CacheSizeValues cacheSizeValues = new CacheSizeValues(processStatName(), getActiveSlaveCount());
-        for (DistStageAck ack : acks) {
-            if (ack instanceof DefaultDistStageAck) {
-                Integer cacheSize = (Integer) ((DefaultDistStageAck) ack).getPayload();
-                cacheSizeValues.setCacheSize(ack.getSlaveIndex(), cacheSize);
-            }
-        }
+   @Override
+   public boolean processAckOnMaster(List<DistStageAck> acks, MasterState masterState) {
+      CacheSizeValues cacheSizeValues = new CacheSizeValues(processStatName(), getActiveSlaveCount());
+      for (DistStageAck ack : acks) {
+         if (ack instanceof DefaultDistStageAck) {
+            Integer cacheSize = (Integer) ((DefaultDistStageAck) ack).getPayload();
+            cacheSizeValues.setCacheSize(ack.getSlaveIndex(), cacheSize);
+         }
+      }
 
-        if (resetOldValues) {
-            masterState.remove("CacheSizeResults");
-        }
+      List<CacheSizeValues> allCacheSizeValues = (List<CacheSizeValues>) masterState.get("CacheSizeResults");
+      if (allCacheSizeValues == null || reset) {
+         allCacheSizeValues = new LinkedList<CacheSizeValues>();
+         masterState.put("CacheSizeResults", allCacheSizeValues);
+      }
+      allCacheSizeValues.add(cacheSizeValues);
 
-        List<CacheSizeValues> allCacheSizeValues = (List<CacheSizeValues>) masterState.get("CacheSizeResults");
-        if (allCacheSizeValues == null) {
-            allCacheSizeValues = new LinkedList<CacheSizeValues>();
-            masterState.put("CacheSizeResults", allCacheSizeValues);
-        }
-        allCacheSizeValues.add(cacheSizeValues);
+      return super.processAckOnMaster(acks, masterState);    //To change body of overridden methods use File | Settings | File Templates.
+   }
 
-        return super.processAckOnMaster(acks, masterState);    //To change body of overridden methods use File | Settings | File Templates.
-    }
+   private String processStatName() {
+      StringBuilder sb = new StringBuilder();
+      String tmp = statName.trim();
+      for (char c : tmp.toCharArray()) {
+         if (Character.isSpaceChar(c)) {
+            sb.append("_");
+         } else {
+            sb.append(c);
+         }
+      }
+      return sb.toString();
+   }
 
-    private String processStatName() {
-        StringBuilder sb = new StringBuilder();
-        String tmp = statName.trim();
-        for (char c : tmp.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                if (sb.length() > 0 && sb.charAt(sb.length() - 1) != '_') {
-                    sb.append("_");
-                }
-                sb.append(c);
-            } else if (Character.isSpaceChar(c)) {
-                sb.append("_");
-            } else if (Character.isLowerCase(c)) {
-                sb.append(Character.toUpperCase(c));
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
+   @Override
+   public String toString() {
+      return "CacheSizeStage{" +
+            "statName='" + statName + '\'' +
+            ", reset=" + reset +
+            '}';
+   }
 
-    public void setStatName(String statName) {
-        this.statName = statName;
-    }
+   public void setStatName(String statName) {
+      this.statName = statName;
+   }
 
-    public void setResetOldValues(boolean resetOldValues) {
-        this.resetOldValues = resetOldValues;
-    }
+
+   public void setReset(boolean reset) {
+      this.reset = reset;
+   }
 }
